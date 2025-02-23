26d0137b1bfa2e40822599777507e92c481cdc88
Alan <alanbustox46@gmail.com>
2025-02-23 03:27:48 -0300

    feat: add ServeStaticModule for serving static files; update package.json and app.module.ts; include product images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { ProductsModule } from './products/products.module';
+
 import { FilesModule } from './files/files.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -19,6 +23,10 @@ import { FilesModule } from './files/files.module';
       database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     ProductsModule,
